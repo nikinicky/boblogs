9a777bbbd4cd97fd5e74a6df05f64e3d1e441b08
nickiesiva <nickiesiva@gmail.com>
2015-02-27 20:49:33 +0700

    Initial Commit

---
+++ app/Http/routes.php
@@ -0,0 +1,4 @@
+<?php
+
+
+Route::get('about', 'PagesController@about');
---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+
+<html>
+  <head>
+    <title>Mooooo</title>
+  </head>
+
+  <body>
+    <h1>About me</h1>
+  </body>
+</html>
